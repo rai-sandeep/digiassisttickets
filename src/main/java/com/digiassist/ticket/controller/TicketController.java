8bab198fa91245e7934b0e9c4952ae048ee47616
sandeepksd <sandeepksd@gmail.com>
2022-07-06 14:11:46 +0530

    Enable cross origin

--- src/main/java/com/digiassist/ticket/controller/TicketController.java
+++ src/main/java/com/digiassist/ticket/controller/TicketController.java
@@ -1,5 +1,6 @@
 package com.digiassist.ticket.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class TicketController {
