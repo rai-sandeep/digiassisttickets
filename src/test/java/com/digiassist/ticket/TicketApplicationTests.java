c250210138eccb1b4139753c14d86cea8afdbde2
Prateek1105 <prateek340ps@gmail.com>
2022-07-13 21:26:19 +0530

    added spring security using JWT

--- src/test/java/com/digiassist/ticket/TicketApplicationTests.java
+++ src/test/java/com/digiassist/ticket/TicketApplicationTests.java
@@ -1,11 +1,12 @@
-/*
- * package com.digiassist.ticket;
- * 
- * import org.junit.jupiter.api.Test;
- * 
- * class TicketApplicationTests {
- * 
- * @Test void contextLoads() { }
- * 
- * }
- */
+
+package com.digiassist.ticket;
+
+import org.junit.jupiter.api.Test;
+
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
