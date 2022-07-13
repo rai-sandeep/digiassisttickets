fef439ea9b29bb01aefb36e02f9539fa6116fda2
Prateek1105 <prateek340ps@gmail.com>
2022-07-13 21:23:14 +0530

    added spring security using JWT

--- src/main/java/com/digiassist/ticket/TicketApplication.java
+++ src/main/java/com/digiassist/ticket/TicketApplication.java
@@ -3,7 +3,7 @@ package com.digiassist.ticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.digiassist.auth", "com.digiassist.ticket"})
 public class TicketApplication {
 
 	public static void main(String[] args) {
